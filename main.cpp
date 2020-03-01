12694f7dd2ea8e007942e60d520e336acf668373
Daniel Stahl <danny.stahl@gmail.com>
2020-02-29 23:28:30 -0800

    algorithm converts string of binary to signed int and checks for carry over and overflow

--- main.cpp
+++ main.cpp
@@ -13,51 +13,63 @@ string add(string a, string b, int & flags);
 int main() {
     int num = 0;
     
-    cout << add("0110", "1010", num);
+    cout << add("1111", "1111", num);
 }
 
 string add(string a, string b, int & flags) {
     string ret;
     int signedNumA = 0;
     int signedNumB = 0;
-    int unsignedNumA = 0;
-    int unsignedNumB = 0;
     int N = 0;
     int Z = 0;
     int C = 0;
     int V = 0;
     int val = 0;
+    string total = "0000";
+    int totalNum = 0;
     
+    // check for carry over
+    for (int x = 3; x >= 0; x--) {
+        int num = a[x] - '0' + b[x] - '0';
+        num += C;
+        
+        if (num == 1) {
+            C = 0;
+            total[x] = '1';
+            
+        } else if(num == 2) {
+            C = 1;
+            total[x] = '0';
+            
+        } else if(num == 3) {
+            C = 1;
+            total[x] = '1';
+        } else {
+            C = 0;
+        }
+        
+    }
+    
+    // Get signed nums
     for (int x = 3; x >= 1; x--) {
         if (a[x] == '1') {
             signedNumA += static_cast<int>(pow(2, val));
         }
-        
+
         if (b[x] == '1') {
             signedNumB += static_cast<int>(pow(2, val));
+        }
+
+        if (total[x] == '1') {
+            totalNum += static_cast<int>(pow(2, val));
         }
         
         val++;
     }
     
-    for (int x = 3; x >= 0; x--) {
-        int num = a[x] - '0' + b[x] - '0';
-        num += C;
-        if (num == 2) {
-            C = 1;
-            cout << 2 << endl;
-        } else if(num == 3) {
-            C = 1;
-            cout << 2 << endl;
-        } else {
-            C = 0;
-        }
+    if (total[0] == '1') {
+        totalNum = totalNum - pow(2, 3);
     }
-    
-    if (C == 1) {
-        cout << "Carry over = 1" << endl;
-    }
-    
     
     if (a[0] == '1') {
         signedNumA = signedNumA - pow(2, 3);
@@ -66,15 +78,28 @@ string add(string a, string b, int & flags) {
     if (b[0] == '1') {
         signedNumB = signedNumB - pow(2, 3);
     }
-    
-    cout << signedNumA << endl;
-    cout << signedNumB << endl;
-    
-    if (signedNumA + signedNumB == 0) {
+
+    // check signed nums for over flow
+    if (signedNumA + signedNumB == totalNum) {
         V = 0;
     } else {
         V = 1;
     }
+
+    cout << "Total: " << total << endl;
+
+    if (C == 1) {
+        cout << "Carry over = 1" << endl;
+    }
+
+    
+
+    cout << signedNumA << endl;
+    cout << signedNumB << endl;
+    cout << totalNum << endl;
+    cout << total << endl;
+    
+   
     
     
     return to_string(V) + to_string(C);
