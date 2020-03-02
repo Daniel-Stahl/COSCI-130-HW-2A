a85791fad50b1ec725085dff47e99b26b079cd27
Daniel Stahl <danny.stahl@gmail.com>
2020-03-01 23:46:06 -0800

    Preparing to turn in

--- main.cpp
+++ main.cpp
@@ -17,7 +17,6 @@ int main() {
 }
 
 string add(string a, string b, int & flags) {
-    string ret;
     int signedNumA = 0;
     int signedNumB = 0;
     int N = 0;
@@ -91,8 +90,6 @@ string add(string a, string b, int & flags) {
     if (total == "0000") {
         Z = 1;
     }
-
-//    cout << "Total: " << total << endl;
     
     flagNum = to_string(N) + to_string(Z) + to_string(V) + to_string(C);
     val = 0;
@@ -108,48 +105,5 @@ string add(string a, string b, int & flags) {
         flags = flags - pow(2, 3);
     }
     
-//    cout << "FLAG: " << flags << endl;
-//    cout << "FLAG NUM: " << flagNum << endl;
-//    cout << signedNumA << endl;
-//    cout << signedNumB << endl;
-//    cout << totalNum << endl;
-//    cout << "total: ";
-    
     return to_string(flags);
 }
-
-/*
-
- Write a function with the following prototype:
- string add(string a, string b, int & flags);
- 
- On entry:
- Awc  (Assume  without  checking)  each  string  param  has  size  exactly  4,  and  each  of  the  4  chars  in both strings is either '0' or '1' (e.g. "0110", "1010").Awc flags is UG (unpredictable garbage) on entry. One of add's jobs is to set this reference arg.add may not assume that flags has been initialized to something sensible on entry.
- 
- add does no i/o (input/ouptut).
- 
- add's job is to return the string corresponding to the 4-bit sum of a and b, and to set flags so that the  high-order  28  bits  are  0  and  the  the  low-order  4  bits  are  the  values  for  N,  Z,  V,  and  C, respectively:
- 
- bit #28: N
- bit #29: Z
- bit #30: V
- bit #31: C
- 
- For example, if a were "0110" and b were "1010":
- 
-  0110
- +1010
-  0000
- N: 0, because the answer we got wasn't negative
- Z: 1, because the answer we got was 0
- V: 0, because with signed (2’s complement) interpretation, 6 + -6 == 0 isn’t wrong, and
- C: 1, because with unsigned interpretation, 6 + 10 == 0 is wrong
- 
- So, the returned string is "0000" (the most significant bit being represented by the first char in the string), and flags is set to 5, because 5 in binary is
- 
-                                   NZVC
- 00000000  00000000  00000000  00000101
- 
- Write a main function that calls your add function. You decide the details of your main.
- 
-*/
